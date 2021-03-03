2956f1b80cb016a981a3db7525251859c133e0f8
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-03 14:41:32 -0500

    added server and made the connection path to the notes.html

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const fs = require("fs");
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+})
