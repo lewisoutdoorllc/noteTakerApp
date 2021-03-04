493b1cb25af27f42343bb84c2894452ec5a70997
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-03-03 19:20:24 -0500

    have to restart having issues with computer saving now

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const uuid = require('uuid')
 const fs = require("fs");
 const path = require("path");
 
@@ -8,13 +9,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
+app.get("/api/notes", (req, res) => {
+  fs.readFile('./db/db.json', 'uft8')
+  console.log('readfile called');
+});
 
 
+app.post("/api/notes", (req, res) => {
+    const newNote = {
+        id: uuid.v4(),
+        reg.body
+    }
+    
+    
+    
+    req.body
+    savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'uft8'));
+    newNote.id = savedNotes.length;
+    savedNotes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'))
+    JSON.stringify(savedNotes)
+    console.log('You have added a new note!!');
+    res.json(newNote)
+    
+  });
+  
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-})
+  console.log(`API server now on port ${PORT}!`);
+});
